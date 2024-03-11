9bba107c58f2df93d160bdd3a759d04d87a61e3e
fan_ren <3289465103@qq.com>
2024-03-11 16:04:24 +0800

    主页html完成，侧边栏完成

--- src/views/discover/singer/home/index.vue
+++ src/views/discover/singer/home/index.vue
@@ -1,13 +1,126 @@
 <template>
-  <div class="home">主页</div>
+  <div class="home">
+    <Container title="入驻歌手">
+      <template #right>
+        <a href="#" class="more">更多 ></a>
+      </template>
+      <template #content>
+        <ul class="singers">
+          <li v-for="n in 10" :key="n">
+            <div class="imgbox">
+              <a href="#" class="mask"></a>
+              <img
+                src="https://p1.music.126.net/cSAmmAvsKhm3N-zxWg7QcQ==/109951168490195225.jpg?param=130y130"
+                alt="" />
+            </div>
+            <p class="des">
+              <a href="#" class="name">张惠妹</a>
+              <a href="#" class="icon icon-user"></a>
+            </p>
+          </li>
+        </ul>
+      </template>
+    </Container>
+    <Container title="热门歌手">
+      <template #content>
+        <ul class="singers">
+          <li v-for="n in 10" :key="n">
+            <div class="imgbox">
+              <a href="#" class="mask"></a>
+              <img
+                src="https://p1.music.126.net/cSAmmAvsKhm3N-zxWg7QcQ==/109951168490195225.jpg?param=130y130"
+                alt="" />
+            </div>
+            <p class="des">
+              <a href="#" class="name">张惠妹</a>
+              <a href="#" class="icon icon-user"></a>
+            </p>
+          </li>
+        </ul>
+        <ul class="artists">
+          <li v-for="n in 50" :key="n">
+            <a href="#">张杰</a>
+          </li>
+        </ul>
+      </template>
+    </Container>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.home {
+  .more {
+    margin-top: 14px;
+    line-height: 16px;
+    display: block;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+  .singers {
+    display: flex;
+    margin: 20px 0 0 -17px;
+    flex-wrap: wrap;
+    li {
+      padding: 0 0 30px 17px;
+    }
+    .imgbox {
+      position: relative;
+      width: 130px;
+      height: 130px;
+      .mask {
+        position: absolute;
+        background-image: url(https://s2.music.126.net/style/web2/img/coverall.png?a69ded6098dfd9babe02d645d4aab8cc);
+        background-position: 0 -680px;
+        width: 100%;
+        height: 100%;
+      }
+      img {
+        display: block;
+        width: 100%;
+        height: 100%;
+      }
+    }
+    p {
+      display: flex;
+      justify-content: space-between;
+      margin-top: 7px;
+      color: #000;
+      align-items: center;
+      .name {
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+      .icon {
+        width: 17px;
+        height: 18px;
+        background-image: url(https://s2.music.126.net/style/web2/img/icon.png?6c1277a1c76f0e472d485d8f916f170c);
+        background-position: 0 -740px;
+      }
+    }
+  }
+  .artists {
+    display: flex;
+    color: #000;
+    border-top: 1px dotted #999;
+    padding-top: 12px;
+    flex-wrap: wrap;
+    li {
+      width: 130px;
+      height: 30px;
+      line-height: 23px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      a:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+}
 </style>
--- src/views/discover/singer/index.vue
+++ src/views/discover/singer/index.vue
@@ -5,66 +5,89 @@
         <h2>推荐</h2>
         <ul class="cates">
           <li>
-            <a href="#" class="active">推荐歌手</a>
+            <router-link to="/discover/artist" exact active-class="active"
+              >推荐歌手</router-link
+            >
           </li>
           <li>
-            <a href="#">入驻歌手</a>
+            <router-link
+              to="/discover/artist/signed"
+              exact
+              active-class="active"
+              >入驻歌手</router-link
+            >
+          </li>
+        </ul>
+      </div>
+      <div class="wrap" v-for="(nav, i) in navs" :key="i">
+        <h2>{{ nav.value }}</h2>
+        <ul class="cates">
+          <li v-for="(cat, index) in nav.cats" :key="index">
+            <a
+              href="#"
+              :class="{ active: matchNav(cat) }"
+              @click.prevent="goTo(cat)"
+              >{{ nav.value
+              }}{{
+                index === 0 ? "男歌手" : index === 1 ? "女歌手" : "组合/乐队"
+              }}</a
+            >
           </li>
         </ul>
       </div>
     </div>
     <div class="content">
       <router-view></router-view>
-      <Container title="入驻歌手">
-        <template #right>
-          <a href="#" class="more">更多 ></a>
-        </template>
-        <template #content>
-          <ul class="singers">
-            <li v-for="n in 10" :key="n">
-              <div class="imgbox">
-                <a href="#" class="mask"></a>
-                <img
-                  src="https://p1.music.126.net/cSAmmAvsKhm3N-zxWg7QcQ==/109951168490195225.jpg?param=130y130"
-                  alt="" />
-              </div>
-              <p class="des">
-                <a href="#" class="name">张惠妹</a>
-                <a href="#" class="icon icon-user"></a>
-              </p>
-            </li>
-          </ul>
-        </template>
-      </Container>
-      <Container title="热门歌手">
-        <template #content>
-          <ul class="singers">
-            <li v-for="n in 10" :key="n">
-              <div class="imgbox">
-                <a href="#" class="mask"></a>
-                <img
-                  src="https://p1.music.126.net/cSAmmAvsKhm3N-zxWg7QcQ==/109951168490195225.jpg?param=130y130"
-                  alt="" />
-              </div>
-              <p class="des">
-                <a href="#" class="name">张惠妹</a>
-                <a href="#" class="icon icon-user"></a>
-              </p>
-            </li>
-          </ul>
-          <ul class="artists">
-            <li v-for="n in 50" :key="n">
-              <a href="#">张杰</a>
-            </li>
-          </ul>
-        </template>
-      </Container>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      navs: [
+        {
+          value: "华语",
+          path: "/cat",
+          cats: ["1001", "1002", "1003"],
+        },
+        {
+          value: "欧美",
+          path: "/cat",
+          cats: ["2001", "2002", "2003"],
+        },
+        {
+          value: "日本",
+          path: "/cat",
+          cats: ["6001", "6002", "6003"],
+        },
+        {
+          value: "韩国",
+          path: "/cat",
+          cats: ["7001", "7002", "7003"],
+        },
+        {
+          value: "其他",
+          path: "/cat",
+          cats: ["4001", "4002", "4003"],
+        },
+      ],
+      cat: "",
+      defaultPath: "/discover/artist/cat",
+    };
+  },
+  
+  methods: {
+    goTo(id) {
+      this.$router.push(`/discover/artist/cat?id=${id}`);
+    },
+    matchNav(cat) {
+      let { path, query } = this.$route;
+      return path === this.defaultPath && query.id == cat;
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
@@ -118,76 +141,6 @@ export default {};
   .content {
     flex: 1;
     padding: 40px;
-
-    .more {
-      margin-top: 14px;
-      line-height: 16px;
-      display: block;
-      &:hover {
-        text-decoration: underline;
-      }
-    }
-    .singers {
-      display: flex;
-      margin: 20px 0 0 -17px;
-      flex-wrap: wrap;
-      li {
-        padding: 0 0 30px 17px;
-      }
-      .imgbox {
-        position: relative;
-        width: 130px;
-        height: 130px;
-        .mask {
-          position: absolute;
-          background-image: url(https://s2.music.126.net/style/web2/img/coverall.png?a69ded6098dfd9babe02d645d4aab8cc);
-          background-position: 0 -680px;
-          width: 100%;
-          height: 100%;
-        }
-        img {
-          display: block;
-          width: 100%;
-          height: 100%;
-        }
-      }
-      p {
-        display: flex;
-        justify-content: space-between;
-        margin-top: 7px;
-        color: #000;
-        align-items: center;
-        .name {
-          &:hover {
-            text-decoration: underline;
-          }
-        }
-        .icon {
-          width: 17px;
-          height: 18px;
-          background-image: url(https://s2.music.126.net/style/web2/img/icon.png?6c1277a1c76f0e472d485d8f916f170c);
-          background-position: 0 -740px;
-        }
-      }
-    }
-    .artists {
-      display: flex;
-      color: #000;
-      border-top: 1px dotted #999;
-      padding-top: 12px;
-      flex-wrap: wrap;
-      li {
-        width: 130px;
-        height: 30px;
-        line-height: 23px;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        a:hover {
-          text-decoration: underline;
-        }
-      }
-    }
   }
 }
 </style>
